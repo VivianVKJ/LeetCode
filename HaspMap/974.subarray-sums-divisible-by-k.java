dda288f9a9436808f3a22d6b50602bc1676f440e
VivianVKJ <1067937107@qq.com>
2020-08-23 21:59:35 -0400

    hashmap, simialar to #560

---
+++ HaspMap/974.subarray-sums-divisible-by-k.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode id=974 lang=java
+ *
+ * [974] Subarray Sums Divisible by K
+ */
+
+// @lc code=start
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        int sum = 0, count = 0;
+        int n = A.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            sum = (sum + A[i]) % K;
+            sum = sum < 0 ? sum + K : sum;
+            if (map.containsKey(sum))
+                count += map.get(sum);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
+// @lc code=end
